b75c9f1813a54343b00c99cd93fd4e173b74e264
unknown <vetimtalaprathyusha@gmail.com>
2022-09-08 17:22:53 +0530

    destructed object

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -30,13 +30,37 @@ const Menu = () => {
     getData();
   }, []);
 
+  const displaydata = (item) => {
+    //alert(item.overrideDuration);
+    /*{
+      Object.keys(item.nutritionLabelling).map((list) =>
+        alert(list, item.nutritionLabelling[list].itemid)
+      );
+    }
+    {
+      data &&
+        data.map((group) =>
+          Object.keys(group.nutritionLabelling).map((list) =>
+            alert(group.nutritionLabelling[list].Item)
+          )
+        );
+    }*/
+    //Object.keys(item.nutritionLabelling)
+    //console.log(item.nutritionLabelling);
+    const data = item.nutritionLabelling;
+    const keys = Object.keys(data.Item);
+    const entries = Object.entries(data.Item);
+    console.log("Nutritional values");
+    entries.forEach((val) => console.log(val[0] + ":" + val[1]));
+    //console.log(nutrition);
+  };
   return (
     <Container style={{ marginTop: "50px" }}>
       <Grid container spacing={5}>
         {data &&
           data.map((item) => (
             <Grid item xs={12} sm={12} md={4} lg={4} key={item.id}>
-              <Card sx={{ maxWidth: 345 }}>
+              <Card sx={{ maxWidth: 345 }} onClick={() => displaydata(item)}>
                 <CardMedia
                   component="img"
                   height="140"
